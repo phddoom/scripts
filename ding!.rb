34dc062a409a64cad6495e75d2632126d2cde8a9
Mitchell Lee Garvin <odin@Asgard.localdomain>
2010-07-19 01:30:54 -0400

    Improvements to ding! use dzen title and slave window set up default dzen settings

--- ding!.rb
+++ ding!.rb
@@ -4,19 +4,28 @@
 #A utility to alert the user that a command/process has completed
 
 require 'getoptlong'
+ENV["POSIXLY_CORRECT"]="true"
+
+def help
+  puts "Long\tShort\tDescription"
+  puts "--help\t-h\tThis help"
+  puts "--dzen\t-d\tPass options as a string to dzen"
+end
 
 opts = GetoptLong.new(
   ["--help", "-h", GetoptLong::NO_ARGUMENT],
   ["--dzen", "-d", GetoptLong::REQUIRED_ARGUMENT]
 )
 
-displayer_args = "-p 10"
+displayer_args = "-p 10 -w 300 -l 4 -sa c -e 'onstart=uncollapse;button1=exit'"
 displayer = "dzen2"
 command = String.new
+
 
 opts.each do |opt, arg|
   case opt
   when "--help"
+    help
     exit
   when "--dzen"
     displayer = "dzen2"
@@ -25,11 +34,9 @@ opts.each do |opt, arg|
 end
 
 
-output = `#{ARGV.first}`
+output = `#{ARGV.join " "}`
 
 status = $?.exitstatus
 
-`echo #{output.dump} | #{displayer} #{displayer_args}`
+`echo -e -n #{output.dump} | #{displayer} #{displayer_args}`
 
-def help
-end
