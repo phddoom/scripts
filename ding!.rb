eec2e4202b1a18a72d086bde3561b22198dff29c
Mitchell Lee Garvin <odin@Midgard.localdomain>
2010-07-19 23:47:07 -0400

    Added ding! a script to alert the user that a command/process is done.

---
+++ ding!.rb
@@ -0,0 +1,35 @@
+#! /usr/bin/env ruby
+
+#ding!
+#A utility to alert the user that a command/process has completed
+
+require 'getoptlong'
+
+opts = GetoptLong.new(
+  ["--help", "-h", GetoptLong::NO_ARGUMENT],
+  ["--dzen", "-d", GetoptLong::REQUIRED_ARGUMENT]
+)
+
+displayer_args = "-p 10"
+displayer = "dzen2"
+command = String.new
+
+opts.each do |opt, arg|
+  case opt
+  when "--help"
+    exit
+  when "--dzen"
+    displayer = "dzen2"
+    displayer_args = arg
+  end
+end
+
+
+output = `#{ARGV.first}`
+
+status = $?.exitstatus
+
+`echo #{output.dump} | #{displayer} #{displayer_args}`
+
+def help
+end
